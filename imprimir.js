736434e9a449754cb7d5347f4031e3b32b279e09
Diana <dianinhadesouzav@gmail.com>
2024-11-26 08:48:30 -0300

    Update imprimir.js

--- imprimir.js
+++ imprimir.js
@@ -1,5 +1,6 @@
 function displaySingleRandomColor(colors) {
   const totalColors = colors.length;
+  
   if (totalColors < 10) {
     console.error("É necessário ter no mínimo 10 cores.");
     return;
@@ -7,9 +8,13 @@ function displaySingleRandomColor(colors) {
 
 let count = 0;
   let selectedColor = null
+  
   while (count < 10) {
-    const randomIndex = Math.floor(Math.random() * 10)) {
-      selectedColor = color;
+    const randomIndex = Math.floor(Math.random() * totalColors);
+     const color = colors[randomIndex];
+    
+      if (count === Math.floor(Math.random() * 10)) {
+        selectedColor = color;
     }
     
     count ++;
