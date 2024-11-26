595bd6a34c5f25bf3b6764e5f5357b22d0671b8a
Diana <dianinhadesouzav@gmail.com>
2024-11-26 08:17:35 -0300

    Update escolha.js

--- escolha.js
+++ escolha.js
@@ -1,1 +1,19 @@
+@param {Array} colors
 
+function displayRandomColors(colors) {
+  const totalColors = colors.length;
+  if (totalColors < 10) {
+    console.error("É necessário ter no mínimo 10 cores.");
+    return;
+  }
+
+let count = 0;
+  while (count < 10) {
+    const randomIndex = Math.floor(Math.random() * totalColors);
+    const color = colors[randomIndex];
+    console.log('Cor ${count + 1}: ${color}');
+    count++;
+  }
+}
+
+displayRandomColors(colors);
